65b9a0e87377cfe610269887296b5f0bdcb4f5b6
minal1593 <minalsalunkhe15@gmail.com>
2023-05-21 20:26:17 +0530

    Added : code to intialize single player at Position 0

--- src/com/Bridgelabz/SnakeLadder.java
+++ src/com/Bridgelabz/SnakeLadder.java
@@ -1,7 +1,8 @@
 package com.Bridgelabz;
 
 public class SnakeLadder {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         System.out.println("Welcome  to snake and Ladder Program");
         int Position = 0;
         System.out.println("Single player at start Position :" +Position);
