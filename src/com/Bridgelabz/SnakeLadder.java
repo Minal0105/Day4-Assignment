a77b8d25785748dbbc1a657cd0cda9e317c30305
minal1593 <minalsalunkhe15@gmail.com>
2023-05-21 20:32:33 +0530

    Added : code to get random number from 1 to 6 using Dice

--- src/com/Bridgelabz/SnakeLadder.java
+++ src/com/Bridgelabz/SnakeLadder.java
@@ -1,11 +1,9 @@
 package com.Bridgelabz;
 
 public class SnakeLadder {
-    public static void main(String[] args)
-    {
-        System.out.println("Welcome  to snake and Ladder Program");
-        int Position = 0;
-        System.out.println("Single player at start Position :" +Position);
+    public static void main(String[] args) {
+        int dice=(int)(Math.random()*6+1);
+        System.out.println("Roll dice Between 1 to 6 :" +dice);
     }
 }
 
