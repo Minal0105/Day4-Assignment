91c0c50a76256c3bc4f67c8857421cec6f5ecf2e
minal1593 <minalsalunkhe15@gmail.com>
2023-05-21 20:39:30 +0530

    Added : code to get check for Options(NoPlay/Ladder/Snake)

--- src/com/Bridgelabz/SnakeLadder.java
+++ src/com/Bridgelabz/SnakeLadder.java
@@ -3,8 +3,29 @@ package com.Bridgelabz;
 public class SnakeLadder {
     public static void main(String[] args)
     {
+        int currentPosition = 10,Position=0;
         int dice=(int)(Math.random()*6+1);
-        System.out.println("Roll dice Between 1 to 6 :" +dice);
+
+        int Options = (int) ((Math.random() * 10) %3);
+
+        switch (Options) {
+            case 0:
+                System.out.println("NoPlay");
+                System.out.println("stay at same position");
+                break;
+            case 1:
+                System.out.println("Ladder");
+                Position=currentPosition+dice;
+                System.out.println("Move ahead by "+dice+" Position");
+                break;
+            default:
+                System.out.println("Snake");
+                Position=currentPosition-dice;
+                System.out.println("Move behind by "+dice+" Position");
+                break;
+        }
+        System.out.println("CurrentPosition : "+Position);
     }
+
 }
 
