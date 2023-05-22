1a1db5695259042194fc50544ecca5e89eeaf8cd
minal1593 <minalsalunkhe15@gmail.com>
2023-05-22 15:51:05 +0530

    Added :code to Play a gaame with two players

--- src/com/Bridgelabz/SnakeLadder.java
+++ src/com/Bridgelabz/SnakeLadder.java
@@ -1,38 +1,92 @@
 package com.Bridgelabz;
 
 public class SnakeLadder {
+    public static final int winningPosition = 100;
+
     public static void main(String[] args) {
-        int Position = 0;
-        int winningPosition=100;
-        int Diceroll=0;
-        for(Position=0;Position<=winningPosition;Position++){
+
+
+        int currentPositionPlayer1 = 0;
+        int currentPositionPlayer2 = 0;
+        boolean player1Turn = true;
+
+        while (currentPositionPlayer1 < winningPosition && currentPositionPlayer2 < winningPosition) {
             int dice=(int)(Math.random()*6+1);
-            Diceroll++;
-            int Options = (int)((Math.random() * 10)%3);
 
-            switch (Options) {
-                case 0:
-                    System.out.println("NoPlay");
-                    break;
-                case 1:
-                    System.out.println("Ladder");
-                    Position+=dice;
-                    break;
-                default:
-                    System.out.println("Snake");
-                    Position-=dice;
+            int Options = (int) ((Math.random() * 10) %3);
 
+            if (player1Turn) {
+                switch (Options) {
+                    case 0:
+                        System.out.println("Player 1 - No Play");
+
+                        break;
+                    case 1:
+                        System.out.println("Player 1 - Ladder");
+                        currentPositionPlayer1 += dice;
+                        break;
+                    case 2:
+                        System.out.println("Player 1 - Snake");
+                        currentPositionPlayer1 -= dice;
+                        if (currentPositionPlayer1 < 0) {
+                            currentPositionPlayer1 = 0;
+                        }
+                        break;
+                    default:
+                        System.out.println("Invalid option");
+                        break;
+                }
+
+                System.out.println("Player 1 - Current Position: " + currentPositionPlayer1);
+            } else {
+                switch (Options) {
+                    case 0:
+                        System.out.println("Player 2 - No Play");
+                        // Player stays in the same position, no change needed
+                        break;
+                    case 1:
+                        System.out.println("Player 2 - Ladder");
+                        currentPositionPlayer2 += dice;
+                        break;
+                    case 2:
+                        System.out.println("Player 2 - Snake");
+                        currentPositionPlayer2 -= dice;
+                        if (currentPositionPlayer2 < 0) {
+                            currentPositionPlayer2 = 0;
+                        }
+                        break;
+                    default:
+                        System.out.println("Invalid option");
+                        break;
+                }
+
+                System.out.println("Player 2 - Current Position: " + currentPositionPlayer2);
             }
-            if (Position < 0) {
-                Position = 0;
-            }else if (Position > winningPosition) {
-                Position -= dice;
+
+
+            if (currentPositionPlayer1 > winningPosition || currentPositionPlayer2 > winningPosition) {
+                if (player1Turn) {
+                    currentPositionPlayer1 -= dice;
+                } else {
+                    currentPositionPlayer2 -= dice;
+                }
+            } else {
+
+                if (Options == 1) {
+                    continue;
+                }
             }
 
-            System.out.println("Number of times the dice was played : "+Diceroll);
-            System.out.println("CurrentPosition : "+Position);
+            player1Turn = !player1Turn;
         }
-        System.out.println("Player has reached the winning position" +winningPosition);
+
+        if (currentPositionPlayer1 >= winningPosition && currentPositionPlayer2 >= winningPosition) {
+            System.out.println("It's a tie!");
+        } else if (currentPositionPlayer1 >= winningPosition) {
+            System.out.println("Player 1 wins!");
+        } else {
+            System.out.println("Player 2 wins!");
+        }
     }
 }
 
